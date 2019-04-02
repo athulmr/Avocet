a7d2c04e7e2b3d498d3450242a38f84343058efa
Athul <athulmr@live.in>
2019-04-02 14:49:15 +0530

    Added createMenu resolver

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,10 +1,12 @@
 
 const ownerResolver = require('./owners');
 const resturantResolver = require('./resturants');
+const menuResolver = require('./menu');
 
 const rootResolver = {
   ...ownerResolver,
   ...resturantResolver,
+  ...menuResolver
 };
 
 module.exports = rootResolver;
---
+++ graphql/resolvers/menu.js
@@ -0,0 +1,31 @@
+const Resturant = require('../../model/resturant');
+const Menu = require('../../model/menu')
+
+module.exports = {
+    createMenu: async args => {
+        try {
+            const menuInput = args.menuInput;
+            console.log(args);
+            const resturant = await Resturant.findById(args.menuInput.resturantId, (err, data) => {
+                return data;
+            });
+            if (resturant.menu) {
+                throw new Error("Already have a Menu tagged with this resturant");
+            }
+            menuInput.resturantId = resturant;
+            const menu = new Menu(menuInput);
+            const result = await menu.save();
+            resturant.menu = result;
+            const updateResturnat = await resturant.save();
+            return {
+                ...result._doc,
+                pwd: null,
+                _id: result.id
+            };
+
+        } catch (err) {
+            return err;
+        }
+
+    }
+}
