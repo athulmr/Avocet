3b1536f10e0e321cff9329576b58932bcb5c7471
athulmr <athulmr@live.in>
2019-08-06 15:04:47 +0530

    Added error message if user name or passoword is incorrect.

--- graphql/app/auth/authData.resolvers.js
+++ graphql/app/auth/authData.resolvers.js
@@ -4,45 +4,51 @@ const jwt = require("jsonwebtoken");
 const Owner = require("../owner/Owner");
 
 module.exports = {
-    /**
-     * Check for authenticity and returns the JWT.
-     * @param {LoginInput} args contains details for login.
-     */
-    login: async args => {
-        const query = JSON.parse(JSON.stringify(args.login));
-        console.log("This is a login query", query);
-        const owner = await Owner.findOne({
-            email: query.userId
-        });
-        let token = "";
+  /**
+   * Check for authenticity and returns the JWT.
+   * @param {LoginInput} args contains details for login.
+   */
+  login: async args => {
+    const query = JSON.parse(JSON.stringify(args.login));
+    console.log("This is a login query", query);
+    const owner = await Owner.findOne({
+      email: query.userId
+    });
+    let token = "";
 
-        // Check if owner exist.
-        if (owner) {
-            // Check if password matches.
-            const isMatch = await bcrypt.compareSync(
-                query.pwd,
-                owner.pwd,
-                (err, isMatch) => {
-                    return isMatch;
-                }
-            );
-
-            if (isMatch) {
-                // Create a token if password is match.
-                token = jwt.sign({
-                    username: query.userId
-                }, "secretKeyUsedForDecrypt", {
-                    expiresIn: "24h"
-                });
-            }
+    // Check if owner exist.
+    if (owner) {
+      // Check if password matches.
+      const isMatch = await bcrypt.compareSync(
+        query.pwd,
+        owner.pwd,
+        (err, isMatch) => {
+          return isMatch;
         }
+      );
 
-        const AuthData = {
-            userId: query.userId,
-            token: token,
-            tokenExpiration: 30
-        };
-
-        return AuthData;
+      if (isMatch) {
+        // Create a token if password is match.
+        token = jwt.sign(
+          {
+            username: query.userId
+          },
+          "secretKeyUsedForDecrypt",
+          {
+            expiresIn: "24h"
+          }
+        );
+      } else {
+        throw new Error("Username or Password incorrect");
+      }
     }
+
+    const AuthData = {
+      userId: query.userId,
+      token: token,
+      tokenExpiration: 30
+    };
+
+    return AuthData;
+  }
 };
